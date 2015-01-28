bf24aad80f80a7122e6cc354bb8dfdc75fa2f7a4
Robert Stiff <Robert.Stiff@easyjet.com>
2015-01-28 13:32:21 +0000

    Allow multiple layouts and cycling of the layout by pressing Ctrl+Alt(+Shift)+Space

--- NMonad/Program.cs
+++ NMonad/Program.cs
@@ -12,27 +12,58 @@ namespace NMonad
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        private static List<Layout> _layouts = new List<Layout>();
+        private static int selectedLayout = 0;
+
+        private static Layout layout
+        {
+            get { return _layouts[selectedLayout]; }
+        }
+
         private static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             var applicationContext = new NMonadApplicationContext();
 
-            HotkeyManager.Current.AddOrReplace("Run", Keys.Control | Keys.Alt | Keys.Space, (s, a) => Run());
+            _layouts.AddRange(new Layout[]
+                {
+                    //new BasicLayout(),
+                    //new ColumnLayout(),
+                    //new FloatingLayout(),
+                    new TallLayout(),
+                    new FullscreenLayout()
+                });
+            
+            HotkeyManager.Current.AddOrReplace("Run", Keys.Control | Keys.Alt | Keys.R, (s, a) => Run());
+            
+            HotkeyManager.Current.AddOrReplace("CycleLayouts", Keys.Control | Keys.Alt | Keys.Space, cycleLayouts);
+            HotkeyManager.Current.AddOrReplace("ReverseCycleLayouts", Keys.Control | Keys.Alt | Keys.Shift | Keys.Space, reverseCycleLayouts);
             
             Timer t = new Timer(state => Run(), null, 0, 100);
             Application.Run(applicationContext);
             t.Dispose();
-
         }
 
+        private static void reverseCycleLayouts(object sender, EventArgs eventArgs)
+        {
+            selectedLayout += 1;
+            if (selectedLayout >= _layouts.Count) selectedLayout = 0;
+            log.InfoFormat("Selected: {0}", layout.GetType().Name);
+        }
+
+        private static void cycleLayouts(object sender, EventArgs eventArgs)
+        {
+            selectedLayout -= 1;
+            if (selectedLayout == -1) selectedLayout = _layouts.Count - 1;
+            log.InfoFormat("Selected: {0}", layout.GetType().Name);
+        }
+
+        
         private static void Run()
         {
             try
             {
-
-                Layout layout = new TallLayout();
-
                 log.InfoFormat("Using layout {0}", layout.GetType().Name);
 
                 string[] ignoredWindows = new[]
