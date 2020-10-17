6784ed052d242bad4824bc920f32ee7ef44b10b3
Robert Stiff <uatecuk@gmail.com>
2020-10-17 13:45:13 +0100

    Simply render a mode of a window layout

--- src/MaterialWindows.TaskBar/ViewModels/MainWindowViewModel.cs
+++ src/MaterialWindows.TaskBar/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,58 @@ namespace MaterialWindows.TaskBar.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Hello World!";
+
+        public List<WindowRow> WindowRows { get; set; } = new List<WindowRow>
+        {
+            new WindowRow
+            {
+                Name = "Web",
+                Windows = new List<Window> 
+                {
+                    new Window
+                    {
+                        Name = "GMail"
+                    },
+                    new Window
+                    {
+                        Name = "Twitter"
+                    },
+                    new Window
+                    {
+                        Name = "Reuters"
+                    }
+                }
+            },
+            new WindowRow
+            {
+                Name = "Messaging",
+                Windows = new List<Window> 
+                {
+                    new Window
+                    {
+                        Name = "Slack"
+                    },
+                    new Window
+                    {
+                        Name = "Discord"
+                    },
+                    new Window
+                    {
+                        Name = "XChat"
+                    }
+                }
+            }
+        };
+    }
+
+    public class WindowRow
+    {
+        public string Name { get; set; }
+        public List<Window> Windows { get; set; } = new List<Window>();
+    }
+
+    public class Window
+    {
+        public string Name { get; set; } = "! Name not set !";
     }
 }
