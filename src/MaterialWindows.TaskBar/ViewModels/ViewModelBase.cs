56a3190887f904c8def78e34af763d7bac0c71e1
Robert Stiff <uatecuk@gmail.com>
2020-10-17 13:21:08 +0100

    Build a graphical application to implement the shell

---
+++ src/MaterialWindows.TaskBar/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace MaterialWindows.TaskBar.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
